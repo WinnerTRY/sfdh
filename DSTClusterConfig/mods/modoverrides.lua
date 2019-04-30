f735645c28892b524ae64fa7a051620c60029ad3
WinnerTRY <47293028+WinnerTRY@users.noreply.github.com>
2019-04-30 22:20:27 +0300

    Update modoverrides.lua

--- DSTClusterConfig/mods/modoverrides.lua
+++ DSTClusterConfig/mods/modoverrides.lua
@@ -1,16 +1,220 @@
--- Use this file to enable and configure your mods. The mod will only be available in the game
--- if you set "enabled=true"!!!
---
--- Also, during the container startup this file will be copied to both Master/ and Caves/ folders. What's setup here
--- will be available in both shards!
---
--- See the example below:
-
 return {
-  -- ["workshop-000000000"]={
-  --   configuration_options={
-  --     ["CustomModSetting"]="value"
-  --   },
-  --   enabled=true
-  -- },
+  ["workshop-1271089343"]={
+    configuration_options={ bekkitt_skeleton_pen=0, null_option=true, skeleton_player=true },
+    enabled=true 
+  },
+  ["workshop-1282814181"]={
+    configuration_options={
+      BT_Health=225,
+      BT_Hunger=225,
+      BT_Sanity=150,
+      SPIDERS=0,
+      Sr_dmg=40,
+      Sr_health=250,
+      WEBBER=0 
+    },
+    enabled=true 
+  },
+  ["workshop-1324709535"]={ configuration_options={  }, enabled=true },
+  ["workshop-1535331014"]={
+    configuration_options={
+      NOFERTILIZE=true,
+      acorn_pl="MEDIUM",
+      berrybush2_pl="MEDIUM",
+      berrybush_juicy_pl="MEDIUM",
+      berrybush_pl="MEDIUM",
+      buildings="NONE",
+      flower_pl="MEDIUM",
+      grass_pl="NORMAL",
+      marblebean_pl="MEDIUM",
+      marsh_bush_pl="NORMAL",
+      pinecone_pl="MEDIUM",
+      sapling_pl="NORMAL",
+      twiggy_tree_pl="MEDIUM" 
+    },
+    enabled=true 
+  },
+  ["workshop-1540284567"]={
+    configuration_options={
+      DropWholeStack=true,
+      MAXSTACKSIZELARGEITEM=99,
+      MAXSTACKSIZEMEDITEM=99,
+      MAXSTACKSIZESMALLITEM=99,
+      auto_stack=true,
+      mobstack="On" 
+    },
+    enabled=true 
+  },
+  ["workshop-1581980167"]={ configuration_options={  }, enabled=true },
+  ["workshop-1600304892"]={ configuration_options={  }, enabled=true },
+  ["workshop-1604299284"]={ configuration_options={ auto_stack=true }, enabled=true },
+  ["workshop-1612927126"]={
+    configuration_options={
+      [""]=0,
+      CODEX1SANITYMULTIPLIER=1,
+      CODEX2SANITYMULTIPLIER=0.85,
+      CODEX3SANITYMULTIPLIER=0.7,
+      MAXWELLHEALING=0.75,
+      MAXWELLHEALTH=100,
+      PETCRAZINESS=5,
+      PETCRAZINESSCAP=60000,
+      READABLECODEX=true,
+      SHADOWDUELISTAI=0,
+      SHADOWDUELISTPENALTY=0.1,
+      SHADOWDUELISTPENALTY2=0.15,
+      SHADOWDUELISTPENALTY3=0.2,
+      SHADOWEQUIPCRAFTING=0,
+      SHADOWWAXWEL2HEALTHREGEN=0.1,
+      SHADOWWAXWEL3HEALTHREGEN=0.1,
+      SHADOWWAXWELHEALTHREGEN=0.1,
+      SHADOWWAXWELL2DAMAGE=42,
+      SHADOWWAXWELL2LIFE=125,
+      SHADOWWAXWELL3DAMAGE=50,
+      SHADOWWAXWELL3LIFE=150,
+      SHADOWWAXWELLDAMAGE=35,
+      SHADOWWAXWELLLIFE=100,
+      SHADOWWAXWELLSPEED=6,
+      SHADOWWORKERPENALTY=0.05,
+      SHADOWWORKERPENALTY2=0.1,
+      SHADOWWORKERPENALTY3=0.15,
+      STARTINGNIGHTMAREFUEL=2,
+      WAXWELLDAPPERNESS=10,
+      WETPENALTY=true,
+      WITHOUTCODEXSANITYMULTIPLIER=1.25 
+    },
+    enabled=true 
+  },
+  ["workshop-1613276969"]={ configuration_options={  }, enabled=true },
+  ["workshop-1628298090"]={
+    configuration_options={
+      chesticeboxconfig=1,
+      excludeitemsconfig=1,
+      includestructures=1,
+      robobee_speed=4,
+      robobeestatuerecipeconfig=2,
+      robobeetechconfig=1,
+      whentoharvest=1 
+    },
+    enabled=true 
+  },
+  ["workshop-356420397"]={ configuration_options={  }, enabled=true },
+  ["workshop-357875628"]={ configuration_options={ ["Wall Gates Recipe"]="gears" }, enabled=true },
+  ["workshop-358297643"]={ configuration_options={  }, enabled=true },
+  ["workshop-361994110"]={
+    configuration_options={ HAT_DECAY=false, RELEASE_BEES=false, SPRING_BEES=true },
+    enabled=true 
+  },
+  ["workshop-370373189"]={
+    configuration_options={ [""]=0, W_Grow="yes", b_seeds="yes", greenhouserecipe="easy" },
+    enabled=true 
+  },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-396026892"]={ configuration_options={ OPT_DIFFICULTY=1 }, enabled=true },
+  ["workshop-398109522"]={ configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
+  ["workshop-426745441"]={
+    configuration_options={
+      unlimited_beargervest=1,
+      unlimited_beefalohat=1,
+      unlimited_beehat=1,
+      unlimited_beequeen=1,
+      unlimited_blue_mushroom=1,
+      unlimited_catcoonhat=1,
+      unlimited_dragonbody=1,
+      unlimited_dragonhead=1,
+      unlimited_dragontail=1,
+      unlimited_earmuffshat=1,
+      unlimited_eyebrellahat=1,
+      unlimited_featherhat=1,
+      unlimited_flowerhat=1,
+      unlimited_goggles=1,
+      unlimited_green_mushroom=1,
+      unlimited_icehat=1,
+      unlimited_molehat=1,
+      unlimited_raincoat=1,
+      unlimited_rainhat=1,
+      unlimited_red_mushroom=1,
+      unlimited_reflectivevest=1,
+      unlimited_spiderhat=1,
+      unlimited_strawhat=1,
+      unlimited_sweatervest=1,
+      unlimited_tophat=1,
+      unlimited_trunkvest=1,
+      unlimited_walrushat=1,
+      unlimited_watermelonhat=1,
+      unlimited_wathgrithr=1,
+      unlimited_winterhat=1 
+    },
+    enabled=true 
+  },
+  ["workshop-426769055"]={
+    configuration_options={
+      disable_revive_penalty_onrespawn=1,
+      disable_skeleton_collision=0,
+      enable_light_near_respawnportal=1,
+      remove_skeleton_on_death=0 
+    },
+    enabled=true 
+  },
+  ["workshop-457071756"]={
+    configuration_options={
+      HUNGER_DRAIN_RATE=1.2,
+      REGENERATION_RATE=32,
+      REGEN_WIDGET_ENABLED=true,
+      REQUIRED_HUNGER_PRECENT=0.66,
+      REQUIRED_SANITY_PERCENT=0.25 
+    },
+    enabled=true 
+  },
+  ["workshop-501385076"]={ configuration_options={  }, enabled=true },
+  ["workshop-509723993"]={
+    configuration_options={ health_enhance=1, health_regen=1, regen_value=100, wall_undeployable=0 },
+    enabled=true 
+  },
+  ["workshop-551324730"]={ configuration_options={  }, enabled=true },
+  ["workshop-569043634"]={ configuration_options={  }, enabled=true },
+  ["workshop-577620478"]={ configuration_options={ ["Batch Size"]=4, Recipe="caves" }, enabled=true },
+  ["workshop-625407906"]={ configuration_options={  }, enabled=true },
+  ["workshop-661253977"]={
+    configuration_options={ amudiao=true, baodiao=1, kong=0, rendiao=2, zbdiao=true },
+    enabled=true 
+  },
+  ["workshop-728459184"]={
+    configuration_options={
+      INCREASEBACKPACKSIZES_BACKPACK=8,
+      INCREASEBACKPACKSIZES_ICEPACK=6,
+      INCREASEBACKPACKSIZES_KRAMPUSSACK=14,
+      INCREASEBACKPACKSIZES_PIGGYBACK=12,
+      largerchester=9,
+      largerdragonflychest=12,
+      largericebox=9,
+      largertreasurechest=9 
+    },
+    enabled=true 
+  },
+  ["workshop-786556008"]={
+    configuration_options={ ENABLEBACKPACK=true, EXTRASLOT=0, INVENTORYSIZE=45 },
+    enabled=true 
+  },
+  ["workshop-907007729"]={
+    configuration_options={
+      AMOUNTBONUS=2,
+      COOKTIME=0.33,
+      FRESHBONUS=0.3,
+      MININGREDIENTS=2,
+      RECIPE="NORMAL" 
+    },
+    enabled=true 
+  } 
 }
